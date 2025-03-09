89e36e3eb7852e414111fdc91496eeae4818157e
Senanee <senanee.w@gmail.com>
2025-03-09 15:07:16 +0000

    #Add Entities, #Adding seed data to dbcontext

--- UKParliament.CodeTest.Data/Data/PersonManagerContext.cs
+++ UKParliament.CodeTest.Data/Data/PersonManagerContext.cs
@@ -18,6 +18,11 @@ public class PersonManagerContext : DbContext
             new Department { Id = 2, Name = "Marketing" },
             new Department { Id = 3, Name = "Finance" },
             new Department { Id = 4, Name = "HR" });
+        modelBuilder.Entity<Person>().HasData(
+            new Person { Id = 1, FirstName = "John", LastName = "Smith", DateOfBirth = new DateTime(1980, 1, 1), DepartmentId = 1 },
+            new Person { Id = 2, FirstName = "Jane", LastName = "Doe", DateOfBirth = new DateTime(1985, 2, 2), DepartmentId = 2 },
+            new Person { Id = 3, FirstName = "James", LastName = "Johnson", DateOfBirth = new DateTime(1990, 3, 3), DepartmentId = 3 },
+            new Person { Id = 4, FirstName = "Jill", LastName = "Brown", DateOfBirth = new DateTime(1995, 4, 4), DepartmentId = 4 });
     }
 
     public DbSet<Person> People { get; set; }
---
+++ UKParliament.CodeTest.Data/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UKParliament.CodeTest.Data;
+
+public class Person
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    public DateTime DateOfBirth { get; set; }
+
+    [Required]
+    public int DepartmentId { get; set; }
+
+    public Department Department { get; set; }
+
+    // Add any additional properties here
+}
